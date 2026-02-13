663a8e32f5c3d770366696c9cee217248e015e97
AnimeIndian-Awards <negativezero338@gmail.com>
2026-02-13 07:42:08 +0000

    fix: minimal OpenNext config, remove broken overrides

--- anime-awards/open-next.config.ts
+++ anime-awards/open-next.config.ts
@@ -1,7 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import wasm from "@opennextjs/cloudflare/overrides/wasm.js";
-import sqlite from "@opennextjs/cloudflare/overrides/sqlite.js";
 
-export default defineCloudflareConfig({
-  overrides: [wasm, sqlite],
-});
+export default defineCloudflareConfig({});

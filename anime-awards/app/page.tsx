5f2a8075f477c5666b9c4df730258a854be5f7a8
AnimeIndian-Awards <negativezero338@gmail.com>
2026-02-18 19:59:48 +0530

    Update page.tsx

--- anime-awards/app/page.tsx
+++ anime-awards/app/page.tsx
@@ -121,7 +121,7 @@ export default function Home() {
             The biggest community event is back. Celebrating the best
             <span className="text-white font-bold"> Hindi Dubs</span>,
             <span className="text-white font-bold"> Theatrical Releases</span>, and
-            <span className="text-white font-bold">{SEASON.animeOf}</span>.
+            <span className="text-white font-bold"> {SEASON.animeOf}</span>.
           </p>
 
           <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
